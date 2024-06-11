a8fe3e485fa86aaab7d75d2d0bfeab7a7a45d7ce
oscarbarritc <73568002+oscarbarritc@users.noreply.github.com>
2024-06-10 18:17:37 -0600

    msg2

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const jsonserver = require('json-server'); 
-const server = jsonserver.create(); 
-const router = jsonserver.router('db.json'); 
-const middlewares = jsonserver.defaults(); 
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
-// a
