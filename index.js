451163274a6eccfd5c289c943d9a7d6a0c45db5a
oscarbarritc <73568002+oscarbarritc@users.noreply.github.com>
2024-08-06 18:42:15 -0600

    Cambio algunos datos

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
+
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log('Corriendo en puerto: '+port);
+});
 // https://sigdeletras.com/2020/crear-una-fake-reat-api-con-json-server-copy/
+
